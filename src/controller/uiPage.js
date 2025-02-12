be98a0abe62146a549f556217849e3dedd58ac4e
konjit <kkll6349@gmail.com>
2025-02-12 14:13:52 +0100

    Fixed the image of the day url.

--- src/controller/uiPage.js
+++ src/controller/uiPage.js
@@ -214,7 +214,6 @@ export const displayNEOs = (noes) => {
 export const getImageOfTheDay = async (imgVideoContainer) => {
   imgVideoContainer.innerHTML = "";
   const imgData = await fetchImageOfTheDay();
-  console.log(imgData);
   if (imgData.error) {
     imgVideoContainer.innerHTML = `<h1 class='error-heading'>${imgData.message}</h1>`;
     console.log('image of the day')
